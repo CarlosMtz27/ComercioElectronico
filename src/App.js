4aa47e018eccac9114ad728991e270d8e0f71cc0
CarlosMtz27 <carlosmartinezhernandez1930@gmail.com>
2025-03-25 12:51:36 -0600

    Primero commit del proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import barraNavegacion from './componentes/BarraNavegacion';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Comercio Electronico</h1>
+      <barraNavegacion />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/BarraNavegacion.jsx
@@ -0,0 +1,12 @@
+//jsx: extensión de JavaScript que permite escribir código similar a HTML dentro de un archivo JavaScript
+import React from "react";
+
+function barraNavegacion(){
+
+    return(
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, error? Ipsa vitae quasi inventore! Quam, ducimus dicta eaque animi expedita officiis voluptatibus ea exercitationem placeat quidem facilis explicabo dolorem laborum!</p>
+        </div>);
+}
+
+export default barraNavegacion;
